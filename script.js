7d6781dee759143f0dceb25803c09313b8e125a4
dakshs20 <dakshsuthar2008@gmail.com>
2025-09-20 12:55:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,13 +24,13 @@ const imageGalleryUrls = [
     "https://iili.io/Fii7T3Q.md.png",
     "https://images.unsplash.com/photo-1664426425021-398a5857217d?q=80&w=1200",
     "https://images.unsplash.com/photo-1664384501410-0a2544280b39?q=80&w=1200",
-    "https://freeimage.host/i/K7DZCXa",
-    "https://freeimage.host/i/K7DpuDP",
-    "https://freeimage.host/i/K7bH3DN",
-    "https://freeimage.host/i/K7bdTnS",
-    "https://freeimage.host/i/K7bdLN4",
-    "https://freeimage.host/i/K7b2c0X",
-    "https://freeimage.host/i/K7b2Dmv"
+    "https://iili.io/K7DZCXa.png",
+    "https://iili.io/K7DpuDP.png",
+    "https://iili.io/K7bH3DN.png",
+    "https://iili.io/K7bdTnS.png",
+    "https://iili.io/K7bdLN4.png",
+    "https://iili.io/K7b2c0X.png",
+    "https://iili.io/K7b2Dmv.png"
 ];
 
 // --- Global State ---
@@ -288,3 +288,4 @@ function startTimer() {
 function stopTimer() {
     clearInterval(timerInterval);
 }
+
